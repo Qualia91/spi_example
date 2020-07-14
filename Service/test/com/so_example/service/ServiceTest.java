f31e5d4f77227b5ef04633c37e9992e65a2e4138
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2020-07-14 11:59:33 +0100

    fixed junit spi testing bug

--- Service/test/com/so_example/service/ServiceTest.java
+++ Service/test/com/so_example/service/ServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import java.util.List;
 
+
+// need to run with this to make it put the modules on the module path
+// -p D:\Software\Programming\projects\Java\spi_example\Service\out\production\Service;D:\Software\Programming\projects\Java\spi_example\Service\out\production\AnotherServiceProvider;D:\Software\Programming\projects\Java\spi_example\Service\out\production\ServiceApi;D:\Software\Programming\projects\Java\spi_example\Service\out\production\ServiceProvider;
 public class ServiceTest {
 
 	@Test
